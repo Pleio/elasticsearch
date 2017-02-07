c289fedf9a72b8b03fe5e1925bd1d811eb547169
bart-j <bart@jeukendrup.nl>
2017-02-07 19:57:43 +0100

    added some missing translation strings

--- languages/en.php
+++ languages/en.php
@@ -10,7 +10,13 @@ $english = array(
     'elasticsearch:settings:profile_fields' => 'Configure searchable profile fields',
 
     'item:comment' => 'Comment',
-    'item:group' => 'Group'
+    'item:group' => 'Group',
+
+    'search_advanced:settings:profile_fields:field' => "Profile field",
+    'search_advanced:settings:user_profile_fields:show_on_form' => "Show on search form",
+    'search_advanced:settings:user_profile_fields:use_autocomplete' => "Search input via an autocomplete",
+    'search_advanced:settings:user_profile_fields:info' => "Allow users to refine their search for users based on profile fields. Currently only text based fields are supported (text, location, url, etc).",
+    'search_advanced:forms:search:user:autocomplete_info' => "Start typing and select from the list"
 );
 
 add_translation("en", $english);
--- languages/nl.php
+++ languages/nl.php
@@ -10,7 +10,13 @@ $dutch = array(
     'elasticsearch:settings:profile_fields' => 'Configureer zoekbare profielvelden',
 
     'item:comment' => 'Reactie',
-    'item:group' => 'Groep'
+    'item:group' => 'Groep',
+
+    'search_advanced:settings:profile_fields' => 'Configureer profielveld specifieke instellingen',
+    'search_advanced:settings:profile_fields:field' => 'Profielveld',
+    'search_advanced:settings:user_profile_fields:show_on_form' => 'Toon op zoek formulier',
+    'search_advanced:settings:user_profile_fields:info' => 'Sta gebruikers toe om de zoekopdracht te verfijnen op basis van profiel velden. Op dit moment worden alleen de tekst gebaseerde velden ondersteund (text, location, url. etc).',
+    'search_advanced:forms:search:user:autocomplete_info' => 'Start met typen en selecteer uit de lijst',
 );
 
 add_translation("nl", $dutch);
