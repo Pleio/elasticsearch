3126af93825a124b9f81d1a3a9078426ff80ac3d
Bart Jeukendrup <bart@jeukendrup.nl>
2017-10-18 13:02:08 +0200

    added boost for title and name

--- classes/ESInterface.php
+++ classes/ESInterface.php
@@ -113,7 +113,7 @@ class ESInterface {
                 'access_id' => array('type' => 'integer'),
                 'site_guid' => array('type' => 'integer'),
                 'subtype' => array('type' => 'integer'),
-                'title' => array('type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
+                'title' => array('boost' => 2, 'type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
                 'description' => array('type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
                 'file_contents' => array('type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
                 'comments' => array('type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
@@ -141,7 +141,7 @@ class ESInterface {
                 'access_id' => array('type' => 'integer'),
                 'site_guid' => array('type' => 'integer'),
                 'subtype' => array('type' => 'integer'),
-                'name' => array('type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
+                'name' => array('boost' => 2, 'type' => 'string', 'analyzer' => 'edge_ngram_analyzer', 'search_analyzer' => 'standard'),
                 'email' => array('type' => 'string', 'index' => 'not_analyzed'),
                 'metadata' => array('type'=> 'nested', 'properties' => array(
                     'access_id' => array('type' => 'integer'),
